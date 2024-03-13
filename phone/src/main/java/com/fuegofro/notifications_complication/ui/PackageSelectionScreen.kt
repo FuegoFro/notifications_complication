39c596d41fb232c1cd68935c0eaefff4929c01e9
Danny Weinberg <FuegoFro@gmail.com>
2024-03-13 15:40:30 -0700

    Add search to the package selection screen
    Also upgrade Gradle, plugin, and library versions

--- phone/src/main/java/com/fuegofro/notifications_complication/ui/PackageSelectionScreen.kt
+++ phone/src/main/java/com/fuegofro/notifications_complication/ui/PackageSelectionScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,8 +22,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.FilterListOff
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ToggleOff
 import androidx.compose.material.icons.filled.ToggleOn
 import androidx.compose.material3.AlertDialog
@@ -30,6 +34,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -44,6 +51,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -129,6 +138,66 @@ fun PackageSelectionScreen(
     }
 
     var filterOnlyEnabled by remember { mutableStateOf(false) }
+    val (currentSearch, setCurrentSearch) = remember { mutableStateOf<String?>(null) }
+    val hasSearch = currentSearch != null
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(hasSearch) {
+        if (hasSearch) {
+            focusRequester.requestFocus()
+        }
+    }
+
+    if (currentSearch != null) {
+        SearchBar(
+            modifier =
+                Modifier.focusRequester(focusRequester)
+                    // The SearchBar is taller than the TopAppBar, plus we're hiding the divider by
+                    // making it the same color as the search bar. In order to prevent the list from
+                    // moving when you open/close search, we shift it up by consuming the top
+                    // insets, 8dp for the difference in height and 1dp for the divider. It's...
+                    // hacky and the resulting text field isn't vertically aligned, but I think it's
+                    // better than the list moving. Short of implementing this all from scratch it's
+                    // probably the best we've got.
+                    .consumeWindowInsets(PaddingValues(top = 9.dp)),
+            query = currentSearch,
+            onQueryChange = setCurrentSearch,
+            onSearch = {},
+            active = true,
+            onActiveChange = { newActive ->
+                if (!newActive) {
+                    setCurrentSearch(null)
+                }
+            },
+            placeholder = { Text(stringResource(id = R.string.package_list_search)) },
+            leadingIcon = {
+                IconButton(onClick = { setCurrentSearch(null) }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = stringResource(id = android.R.string.cancel)
+                    )
+                }
+            },
+            colors =
+                SearchBarDefaults.colors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    dividerColor = MaterialTheme.colorScheme.primaryContainer,
+                )
+        ) {
+            // Need to re-set the background color here, since the SearchBar's containerColor sets
+            // the background for the search field *and* the results view.
+            Surface(modifier = Modifier.fillMaxHeight()) {
+                PackageList(
+                    PaddingValues(),
+                    installedPackages,
+                    enabledPackages,
+                    ::setPackageEnabled,
+                    filterOnlyEnabled,
+                    currentSearch,
+                )
+            }
+        }
+        return
+    }
 
     Scaffold(
         topBar = {
@@ -138,7 +207,7 @@ fun PackageSelectionScreen(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
                         titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
                     ),
-                title = { Text("Pick apps") },
+                title = { Text(stringResource(R.string.package_list_title)) },
                 navigationIcon = {
                     IconButton(onClick = onNavigateUp) {
                         Icon(
@@ -148,21 +217,6 @@ fun PackageSelectionScreen(
                     }
                 },
                 actions = {
-                    if (filterOnlyEnabled) {
-                        AppBarAction(
-                            icon = Icons.Filled.FilterListOff,
-                            labelRes = R.string.package_list_filter_all,
-                        ) {
-                            filterOnlyEnabled = false
-                        }
-                    } else {
-                        AppBarAction(
-                            icon = Icons.Filled.FilterList,
-                            labelRes = R.string.package_list_filter_only_enabled,
-                        ) {
-                            filterOnlyEnabled = true
-                        }
-                    }
                     // Only want to show "enable all" when we have a non-null, empty set
                     if (enabledPackages?.isEmpty() == true) {
                         AppBarAction(
@@ -184,6 +238,27 @@ fun PackageSelectionScreen(
                             setDisableAllDialogVisible(true)
                         }
                     }
+                    if (filterOnlyEnabled) {
+                        AppBarAction(
+                            icon = Icons.Filled.FilterListOff,
+                            labelRes = R.string.package_list_filter_all,
+                        ) {
+                            filterOnlyEnabled = false
+                        }
+                    } else {
+                        AppBarAction(
+                            icon = Icons.Filled.FilterList,
+                            labelRes = R.string.package_list_filter_only_enabled,
+                        ) {
+                            filterOnlyEnabled = true
+                        }
+                    }
+                    AppBarAction(
+                        icon = Icons.Filled.Search,
+                        labelRes = R.string.package_list_search
+                    ) {
+                        setCurrentSearch("")
+                    }
                 },
             )
         },
@@ -194,6 +269,7 @@ fun PackageSelectionScreen(
             enabledPackages,
             ::setPackageEnabled,
             filterOnlyEnabled,
+            filterSearch = null,
         )
         if (disableAllDialogVisible) {
             AlertDialog(
@@ -214,7 +290,7 @@ fun PackageSelectionScreen(
                     TextButton(onClick = { setDisableAllDialogVisible(false) }) {
                         Text(stringResource(id = android.R.string.cancel))
                     }
-                }
+                },
             )
         }
     }
@@ -228,13 +304,15 @@ private fun PackageList(
     installedPackages: List<PackageInfo>?,
     enabledPackages: Set<String>?,
     setPackageEnabled: suspend (String, Boolean) -> Unit,
-    filterOnlyEnabled: Boolean
+    filterOnlyEnabled: Boolean,
+    filterSearch: String?,
 ) {
+    val filterSearchLowered = filterSearch?.lowercase()
     val coroutineScope = rememberCoroutineScope()
 
     if (installedPackages == null || enabledPackages == null) {
         Box(Modifier.fillMaxSize().padding(paddingValues), contentAlignment = Alignment.Center) {
-            Text(text = "Loading...")
+            Text(stringResource(R.string.package_list_loading))
         }
     } else {
         LazyColumn(
@@ -245,7 +323,12 @@ private fun PackageList(
             items(installedPackages, key = { installedPackage -> installedPackage.name }) {
                 packageInfo ->
                 val enabled = enabledPackages.contains(packageInfo.name)
-                if (!filterOnlyEnabled || enabled) {
+                val hideForDisabled = filterOnlyEnabled && !enabled
+                val hideForSearch =
+                    filterSearchLowered != null &&
+                        !packageInfo.label.lowercase().contains(filterSearchLowered)
+
+                if (!hideForDisabled && !hideForSearch) {
                     PackageRow(
                         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
                         packageInfo = packageInfo,
